ecd66f51ffdb2ad7421e7fd6cf3628e4d61b8d6b
Anshul Khandelwal <AnshulKhandelwal02@gmail.com>
2020-10-12 15:24:26 +0530

    add material and animation

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GokuldhamComponent } from './gokuldham/gokuldham.component';
+import { MahasangramComponent } from './mahasangram/mahasangram.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent, pathMatch: 'full' },
+  { path: 'gokuldham', component: GokuldhamComponent },
+  { path: 'mahasangram', component: MahasangramComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
